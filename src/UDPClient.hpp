ad9ab6f6d226b4a301949fed4626fe73bf822dbd
Sheree Morphett <sheree.morphett@gmail.com>
2024-10-01 13:35:09 +0300

    adding conditional windows option

--- src/UDPClient.hpp
+++ src/UDPClient.hpp
@@ -4,7 +4,12 @@
 #include <cstdlib>
 #include <string>
 #include <memory>
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
 #include <arpa/inet.h>
+#endif
 #include <unistd.h>
 
 
